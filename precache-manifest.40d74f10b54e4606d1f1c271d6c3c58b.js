7dd2fdd3a330caf580f21bae1b3f2c99b3b9b005
Welcome Sithole <sitholewb@outlook.com>
2019-06-22 23:23:51 +0200

    try react

---
+++ precache-manifest.40d74f10b54e4606d1f1c271d6c3c58b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2276a6ec9a297499a198",
+    "url": "/static/js/main.2276a6ec.chunk.js"
+  },
+  {
+    "revision": "d1efd2820bd58690135d",
+    "url": "/static/js/2.d1efd282.chunk.js"
+  },
+  {
+    "revision": "2276a6ec9a297499a198",
+    "url": "/static/css/main.11519494.chunk.css"
+  },
+  {
+    "revision": "d1efd2820bd58690135d",
+    "url": "/static/css/2.4859c12b.chunk.css"
+  },
+  {
+    "revision": "02440df206bced305f261b2498dfe72a",
+    "url": "/index.html"
+  }
+];
